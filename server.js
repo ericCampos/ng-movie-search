d02ae579b13d13cb2d40e4c44cf2ece8d4ca29cd
ericCampos <ericcampos12@gmail.com>
2020-05-22 18:05:02 +0200

    Corrected deployed app folder name on server.js

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const app = express();
 
-app.use(express.static('./dist/movie-search'));
-app.get('/*', function(req, res) {
-	res.sendFile('index.html', { root: './dist/movie-search' });
+app.use(express.static("./dist/movieSearch"));
+app.get("/*", function (req, res) {
+  res.sendFile("index.html", { root: "./dist/movieSearch" });
 });
 
 app.listen(process.env.PORT || 8080);
